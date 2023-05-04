64e020678763d11cb14dce8365a2c8e2369a26f4
hutao1 <hutao1@akulaku.com>
2023-05-04 20:22:53 +0800

    feat: pack

---
+++ src/get-pack-files.js
@@ -0,0 +1,48 @@
+const fs = require("fs")
+const arg = require("arg")
+const { promisify } = require("util")
+const readdir = promisify(fs.readdir)
+const bundled = promisify(require("./npm-bundled"))
+
+// 获取 pack 内容
+async function getPackFiles(packPath, nodeModulesPath) {
+  try {
+    // 1. 获取基础文件
+    const fileList = (await readdir(packPath)).filter(filterFilePath).map(item => `${packPath}/${item}`)
+
+    // 2. 获取node_modules内容
+    const moduleList = await getModuleList({ path: packPath, nodeModulesPath: nodeModulesPath || process.cwd(), bundleDependenciesKey: "dependencies" });
+    return [...fileList, ...moduleList];
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function filterFilePath(path) {
+  return !/node_modules/.test(path)
+}
+
+async function getModuleList(opt) {
+  const moduleList = (
+    await bundled({ path: opt.path, nodeModulesPath: opt.nodeModulesPath, bundleDependenciesKey: opt.bundleDependenciesKey })
+  )
+  for (let index = 0; index < moduleList.length; index++) {
+    const item = moduleList[index];
+    moduleList[index] = await getRealPath(`${opt.nodeModulesPath}/node_modules/${item}`)
+  }
+  return moduleList;
+}
+
+async function getRealPath(path) {
+  return new Promise((resolve) => {
+    // We need to resolve the real native path for case-insensitive file systems.
+    // For example, we can access file as C:\Code\Project as well as c:\code\projects
+    // Without this we can face a problem when try to install packages with -w flag,
+    // when root dir is using c:\code\projects but packages were found by C:\Code\Project
+    fs.realpath.native(path, function (err, resolvedPath) {
+      resolve(err !== null ? path : resolvedPath)
+    })
+  })
+}
+
+module.exports = getPackFiles;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,61 @@
-// const bundled = require('npm-bundled')
 const tar = require('tar')
 const fs = require("fs")
+const arg = require('arg');
+const fg = require('fast-glob');
+const path = require('path');
+const getPackFiles = require("./get-pack-files");
 
-const optionConfig = {
-  "workspace": null,
-};
+const args = arg({
+  '--workspace': String,
+})
 
-const argvObj = process.argv.slice(2).reduce((obj, str) => {
-  if (str.startsWith(`--`)) {
-    const [key, value] = str.split("=");
-    const optionKey = key.split("--")[1];
-    if (optionKey in optionConfig) {
-      optionConfig[optionKey] = value;
+async function start() {
+  try {
+    const rootPkg = require(`${process.cwd()}/package.json`);
+    const workspace = args["--workspace"];
+    // 1. 获取包路径
+    let pkgDir = process.cwd();
+    if(workspace) {
+      // workspace
+      pkgDir = await findWorkspaceDir(rootPkg.workspaces || [], workspace);
+    } else {
+      // root
     }
+
+    // 2. 获取 pack 内容
+    const packFiles = await getPackFiles(pkgDir);
+
+    console.log(packFiles)
+
+    // npm/yarn/pnpm pack 暂时先支持npm
+    tar.c({
+      gzip: true,
+      prefix: "test",
+    }, [...packFiles]).pipe(fs.createWriteStream('my-tarball.tgz')).on("close", () => {
+      process.exit(0)
+    });
+
+  } catch (error) {
+    console.log(error)
   }
-  return obj
-}, {})
-
-function start() {
-  const workspace = argvObj.workspace;
-  const spawn = require("child_process").spawn;
-  const pack = spawn("npm", [`pack`, `--workspace=${workspace}`]);
-  pack.on("close", (code) => {
-    console.log(code)
-  })
-
-  // const pkg = require(`${process.cwd()}${workspace ? `/${workspace}` : "" }/package.json`)
-  // // async version
-  // bundled({ path: process.cwd() }, (er, list) => {
-  //   if(!er || !list.length) {
-  //     console.log(`no files`)
-  //     process.exit(0);
-  //     return;
-  //   }
-  //   const modules = list.map(item => `node_modules/${item}`)
-  //   tar.c({
-  //     gzip: true,
-  //     prefix: "test",
-  //   }, [...modules]).pipe(fs.createWriteStream('my-tarball.tgz')).on("close", () => {
-  //     process.exit(0)
-  //   });
-  // })
 }
 
 start();
+
+async function findWorkspaceDir (workspaces, workspace) {
+  let i = 0;
+  while (i < workspaces.length) {
+    const isDir = workspaces[i].split("/")[1] !== undefined;
+    let pkgPath;
+    if(isDir) {
+      const workspaceItem = workspaces[i].endsWith("/*") ? `${workspaces[i]}*` : workspaces[i];
+      pkgPath = (await fg([process.cwd(), `./${workspaceItem}/${workspace}/package.json`]))[0];
+    } else if(workspace === workspaces[i] && fs.existsSync(path.resolve(process.cwd(), `./${workspace}/package.json`))) {
+      pkgPath = `./${workspace}/package.json`;
+    }
+    if (pkgPath) {
+      return path.resolve(process.cwd(), pkgPath.replace("/package.json", ""));
+    }
+    i++;
+  }
+}
